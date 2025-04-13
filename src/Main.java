711a891fe9419ee54a816427a9ed600c8bee9d83
Urmat <urmat.almasbekov@alatoo.edu.kg>
2025-04-13 20:42:17 +0600

    111

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print(" введите количество строк (нечетное, >= 5): ");
+        System.out.print(" Введите количество строк (нечетное, >= 5): ");
         int rows = scanner.nextInt();
         System.out.print(" Введите количество столбцов (нечетное, >= 5): ");
         int cols = scanner.nextInt();
